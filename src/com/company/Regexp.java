721e7dfde5dc04ce7b97c190e22f512e653ba2c2
Nikita1801 <nikita-makarevich@mail.ru>
2022-05-09 12:38:26 +0300

    Lexer Done

--- src/com/company/Regexp.java
+++ src/com/company/Regexp.java
@@ -7,6 +7,7 @@ public class Regexp {
 
 
     private static Map<String, Pattern> lex = new HashMap<>();
+    private static List<String> charsToRemove = new ArrayList<>();
 
     static {
 
@@ -19,6 +20,9 @@ public class Regexp {
         lex.put("COMMENT", Pattern.compile("//"));
         lex.put("ASSIGN", Pattern.compile("="));
         lex.put("ADD", Pattern.compile("\\+"));
+
+
+        charsToRemove.add(" ");
     }
 
 
@@ -30,33 +34,77 @@ public class Regexp {
         List<String> stringList = new LinkedList<>();
         List<Token> tokenList = new LinkedList<>();
 
-        for (int word = 0; true; word++){
-            stringList.add(word, scanner.nextLine());
-            Boolean checkIsEmpty = stringList.get(word).isEmpty();
-            if (checkIsEmpty){
-                stringList.remove(word);
+        for (int line = 0; true; line++) {
+            stringList.add(line, scanner.nextLine());
+            Boolean checkIsEmpty = stringList.get(line).isEmpty();
+            if (checkIsEmpty) {
+                stringList.remove(line);
                 break;
             }
         }
         for (int position = 0; position < stringList.size(); position++) {
-            for (String key : lex.keySet()) {
-                Matcher matcher;
-                matcher= lex.get(key).matcher(stringList.get(position));
 
+            String commandLine = stringList.get(position);
+//            commandLine = prepareString(commandLine);
+//            System.out.println(commandLine);
 
-                while (matcher.find()) {
-                    String currentValue = stringList.get(position).substring(matcher.start(), matcher.end());
-                    tokenList.add(new Token(key, currentValue));
+            String acc = "";
+            for (int i = commandLine.length() - 1; i >= 0; i--) {
+                char c = commandLine.charAt(i);
 
-
+                //accum all symbols app to whitespace and skip all trailing whitespaces
+                for (int k = 0; true; k++) {
+                    if (i - k >= 0 && commandLine.charAt(i - k) != ' ') {
+                        acc += commandLine.charAt(i-k);
+                    }else {
+                        i=i-k;
+                        break;
+                    }
                 }
 
+                //prepare accumulator before  matching against regexp
+                StringBuilder builder = new StringBuilder();
+                builder.append(acc);
+                acc = builder.reverse().toString();
+                // System.out.println("I'm current accumulator: " + acc);
+
+                //found tokens in accum
+                for (String key : lex.keySet()) {
+                    Matcher matcher;
+                    matcher = lex.get(key).matcher(acc);
+
+
+                    while (matcher.find()) {
+                        String currentValue = acc.substring(matcher.start(), matcher.end());
+                        tokenList.add(new Token(key, currentValue));
+                    }
+
+                }
+                acc="";
+
             }
+
+
+
+
         }
-        for (Token token: tokenList) {
+        for (Token token : tokenList) {
             System.out.println(token);
         }
 
+    }
+
+    /***
+     * Deletes all not neseccesary symbols in command line of programm
+     * @param commandLine
+     * @return
+     */
+    private static String prepareString(String commandLine) {
+        String res = commandLine;
+        for (String str : charsToRemove) {
+            res = commandLine.replace(str, "");
+        }
+        return res;
     }
 }
 
@@ -65,7 +113,7 @@ class Token {
     private String type;
     private String value;
 
-    public Token(String type, String value){
+    public Token(String type, String value) {
         this.type = type;
         this.value = value;
 
