35c4786c9039f5005e685762862228c47519611f
Nikita1801 <nikita-makarevich@mail.ru>
2022-05-23 00:51:58 +0300

    Lexer + Parser + Interpretator

---
+++ src/com/company/VarHolder.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class VarHolder {
+    public double value;
+    public String name;
+
+    public VarHolder(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+}
